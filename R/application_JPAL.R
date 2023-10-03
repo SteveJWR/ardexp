21a8deac46d5ac8b66360f927546572f7b1d3fc2
SteveJWR <steven.wilkins.reeves@gmail.com>
2023-10-03 13:32:32 -0700

    Increasing number of simulations per village

--- R/application_JPAL.R
+++ R/application_JPAL.R
@@ -133,14 +133,14 @@ if(estimate.sbm){
 #Global average treatment effect
 theta = gamma1 - gamma0
 
-n.sims =  100#
+n.sims =  1000#
 
 # Only compare the full data and partial data versions
 n.methods = 3
 results <- array(NA, c(n.sims, n.methods))
 
 # Methods tuning parameters
-B.boot = 50
+B.boot = 500
 #### Simulations Start
 
 # formula for the regression model
