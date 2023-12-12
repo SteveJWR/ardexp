c64f3594ade502ff852f13196bed7ddaf74ba185
SteveJWR <steven.wilkins.reeves@gmail.com>
2023-12-12 13:08:33 -0800

    add package installs

--- R/install_packages.R
+++ R/install_packages.R
@@ -1,2 +1,6 @@
-install.packages("blockmodels",repos = "http://cran.us.r-project.org")
-library(blockmodels)
+#install.packages("blockmodels",repos = "http://cran.us.r-project.org")
+install.packages("haven",repos = "http://cran.us.r-project.org")
+install.packages("R.matlab",repos = "http://cran.us.r-project.org")
+#library(blockmodels)
+library(haven)
+library(R.matlab)
